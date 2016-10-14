7bb3e12f3639fe1a51f0f9c8571a14618f367f13
N.Shimizu <chikoski@gmail.com>
2016-10-14 18:07:03 +0900

    cat の基本機能を実装

--- samples/read_file/src/main.rs
+++ samples/read_file/src/main.rs
@@ -1,9 +1,22 @@
 use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn read_file(filename:String) -> Result<String, io::Error>{
+    let mut file = try!(File::open(filename));
+    let mut content = String::new();
+    try!(file.read_to_string(&mut content));
+    Ok(content)
+}
 
 fn main() {
     let args:Vec<String> = env::args().collect();
 
     if args.len() > 1{
-        println!("{}", args[1]);
+        println!("{}", match read_file(args[1].clone()){
+            Ok(content) => content,
+            Err(reason) => panic!(reason) 
+        });
     }
 }
