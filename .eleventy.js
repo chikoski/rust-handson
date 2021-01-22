f3f72f6ca9acab9422a4e087f39ff3fc6382cc0b
N.Shimizu <chikoski@gmail.com>
2021-01-22 09:28:49 +0900

    remove an unused plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require("markdown-it");
-//const markdownItSections = require("markdown-it-header-sections");
 const createSlide = require("./plugins/create-slide");
 
 module.exports = function(eleventyConfig) {
